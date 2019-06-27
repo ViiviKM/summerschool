32fcd743911e4664602d287bbe99f7dc51e45403
Viivi Kallio-Myers <viivi.kallio-myers@fmi.fi>
2019-06-27 15:26:37 +0300

    functions and overloads added.

--- programming/fortran/vecmod/vecmod.F90
+++ programming/fortran/vecmod/vecmod.F90
@@ -7,15 +7,67 @@ module vector_algebra
 
   ! TODO: overload operators needed by the parser
 
-  ! ...
+  interface operator(+)
+     module procedure vector_sum
+  end interface operator(+)
 
+  interface operator(-)
+     module procedure vector_difference
+  end interface operator(-)
+
+  interface operator(*)
+     module procedure vector_product
+  end interface operator(*)
+  
+  interface operator(.x.)
+     module procedure vector_cross_product
+  end interface operator(.x.)
+
+  interface abs
+     module procedure vector_norm
+  end interface abs
+  
+  
+  
 contains
   ! TODO: implement the corresponding functions
 
   function vector_sum(v1, v2) result(v3)
-    !    ...
+    type(vector_t), intent(in) :: v1,v2
+    type(vector_t) :: v3
+    v3%x = v1%x + v2%x
+    v3%y = v1%y + v2%y
+    v3%z = v1%z + v2%z
   end function vector_sum
 
-  ! ...
+  function vector_difference(v1,v2) result(v3)
+    type(vector_t), intent(in) :: v1,v2
+    type(vector_t) :: v3
+    v3%x = v1%x - v2%x
+    v3%y = v1%y - v2%y
+    v3%z = v1%z - v2%z
+  end function vector_difference
 
+  function vector_product(v1,v2) result(v3)
+    type(vector_t), intent(in) :: v1,v2
+    type(vector_t) :: v3
+    v3=v1%x*v2%x + v1%y*v2%y + v1%z*v2%z
+  end function vector_product
+  
+  function vector_cross_product(v1,v2) result(v3)
+    type(vector_t), intent(in) :: v1,v2
+    type(vector_t) :: v3
+    v3%x=v1%y*v2%z-v1%z*v2%y
+    v3%y=v1%z*v2%x-v1%x*v2%z
+    v3%z=v1%x*v2%y-v1%y*v2%x    
+  end function vector_cross_product
+
+  function vector_norm(v) result(r)
+    type(vector_t) :: v
+    real(REAL64) :: r
+    r = sqrt(v%x**2 + v%y**2 + v%z**2))
+  end function vector_norm
+  
+  
+  
 end module vector_algebra
