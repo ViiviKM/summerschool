df4ece10a66ad943c534c285611fca59504cd6d8
Viivi Kallio-Myers <viivi.kallio-myers@fmi.fi>
2019-06-28 10:27:46 +0300

    hello process printing added

--- mpi/hello-world/hello.f90
+++ mpi/hello-world/hello.f90
@@ -3,9 +3,20 @@ program hello
   implicit none
 
   integer :: rc
+  integer :: rank, size
+  
   call MPI_Init(rc)
 
+  call Mpi_Comm_rank(MPI_COMM_WORLD, rank, rc)
+  
   write(*,*) 'Hello world'
+  write(*,*) rank
+
+  ! printing number of processes
+  if (rank == 0) then
+     call MPI_Comm_size(MPI_COMM_WORLD, size, rc)
+     write(*,*) size
+  end if
 
   call MPI_Finalize(rc)
 
