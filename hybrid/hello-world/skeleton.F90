9616992d0db0b882537ce34fa15aa41b1255f453
Viivi Kallio-Myers <viivi.kallio-myers@fmi.fi>
2019-07-01 15:18:58 +0300

    hello world with threads

--- hybrid/hello-world/skeleton.F90
+++ hybrid/hello-world/skeleton.F90
@@ -6,17 +6,19 @@ program hello
   integer :: provided, required=MPI_THREAD_FUNNELED
   integer :: omp_rank
 
-  call MPI_init(rc)
-  
-  !$omp parallel private(omp_rank)
-  omp_rank = omp_get_thread_num()
-  print *, 'Hello world! by thread ', omp_rank
-
-
-
   ! TODO: Initialize MPI with thread support. 
+  call mpi_init_thread(required, provided, rc)
+
+  
   ! TODO: Find out the MPI rank and thread ID of each thread and print
   !       out the results.
+  !$omp parallel private(omp_rank)
+  call mpi_comm_rank(mpi_comm_world, my_id, rc)
+  omp_rank = omp_get_thread_num()
+  
+  print *, 'Hello world! by thread ', omp_rank, &
+       ' process: ', my_id
+  
 
   ! TODO: Investigate the provided thread support level.
 
