31c7b812725e9d2bb7c2d3456b8a1e487fe61d99
Viivi Kallio-Myers <viivi.kallio-myers@fmi.fi>
2019-07-01 15:50:42 +0300

    variables with various privacies.

--- hybrid/data-sharing/variables.F90
+++ hybrid/data-sharing/variables.F90
@@ -1,14 +1,19 @@
 program exer1
+  use omp_lib
   implicit none
   integer :: var1, var2
   var1 = 1
   var2 = 2
 
   ! TODO:
+  !$omp parallel default(shared) firstprivate(var1, var2)
+  
   !   Test different data sharing clauses here
   print *, 'Region 1:       var1=', var1, 'var2=', var2
   var1 = var1 + 1
   var2 = var2 + 1
+
+  !$omp end parallel
   !end here :)
   print *, 'After region 1: var1=', var1, 'var2=', var2
   print *
